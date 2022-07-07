95e29946e6136ecfad330f5ec3e1218f3661f8ad
Fahad Khan <fahad@Voyager.com>
2022-07-07 17:18:31 +0500

    worked on hotfix

--- CoreUI/src/app/shared/components/bookings/bookings.component.ts
+++ CoreUI/src/app/shared/components/bookings/bookings.component.ts
@@ -105,6 +105,8 @@ export class BookingsComponent extends AbstractGenericComponent implements OnIni
 
     dateToPlaceHolder: string = 'Booking Date';
     dateFromPlaceHolder: string = 'Booking Date';
+
+    longDateFormat: string = "";
     // #endregion
 
     constructor(
@@ -205,6 +207,7 @@ export class BookingsComponent extends AbstractGenericComponent implements OnIni
 
     async getCurrentBranchDetail() {
         const branch = await super.getBranchDetail(this._dataSharingService);
+        this.longDateFormat = await super.getBranchDateFormat(this._dataSharingService, ENU_DateFormatName.LongDateFormat);
         if (branch && branch.hasOwnProperty("Currency")) {
             this.currencyFormat = branch.CurrencySymbol;
         }
